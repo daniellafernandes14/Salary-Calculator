db2e1475521976736db39974b94e4b6305974958
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-03-21 21:33:28 +0000

    Updating if else statement for different salaries and tax brackets

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-// Select the salary input
-const
-// Select the Calculate button
+// Select the form
+const form = document.getElementById('salary-form');
+console.log(form);
 // Calculate the salary after tax (take into consideration 40k+ salaries)
+form.addEventListener('submit', showIncome);
+
+function showIncome(e) {
+  // Select the salary input
+  const salary = parseFloat(document.getElementById('salary').value);
+  const allowanceRemoved = (salary - 12570);
+  if(salary <= 12570) {
+    // return salary
+  } else if(salary <= 50270) {
+    // 20% off allowanceRemoved
+  } else if(salary <= 125140) {
+    // 40% off allowanceRemoved
+  } else if(salary <= 150000) {
+    // 40% off salary
+  } else {
+    // 45% off salary
+  }
+  e.preventDefault();
+}
 // Create an element to prepend to "Montly take home: h6"
 // Update the salary after tax in this new element
 // Add a pie chart that gets automated with %s of what you want to spend your money on
