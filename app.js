746ca56ff5a753bf4489030b38b30ee93322c2a5
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-03-21 21:07:44 +0000

    pseudo coded app.js with all parts I want to complete

---
+++ app.js
@@ -0,0 +1,7 @@
+// Select the salary input
+const
+// Select the Calculate button
+// Calculate the salary after tax (take into consideration 40k+ salaries)
+// Create an element to prepend to "Montly take home: h6"
+// Update the salary after tax in this new element
+// Add a pie chart that gets automated with %s of what you want to spend your money on
