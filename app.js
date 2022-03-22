1159426290c3dabcb8f9b28662f3116fa313380d
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-03-22 11:56:15 +0000

    Installed chart.js to page to render a pie chart with monthly outgoings

--- app.js
+++ app.js
@@ -27,26 +27,38 @@ function showIncome(e) {
   if(salary <= 12570) {
     // return salary
     const finalOutcome = (salary / 12).toFixed(0)
-    resultsPara.innerHTML = `£${finalOutcome}`;
+    resultsPara.innerHTML = finalOutcome;
   } else if(salary <= 50270) {
     // 20% off allowanceRemoved
     const finalOutcome = (((allowanceRemoved * 0.8) + 12570) / 12).toFixed(0);
-    resultsPara.innerHTML = `£${finalOutcome}`;
+    resultsPara.innerHTML = finalOutcome;
   } else if(salary <= 125140) {
     // 40% off allowanceRemoved
     const finalOutcome = (((allowanceRemoved * 0.6) + 12570) / 12).toFixed(0);
-    resultsPara.innerHTML = `£${finalOutcome}`;
+    resultsPara.innerHTML = finalOutcome;
   } else if(salary <= 150000) {
     // 40% off salary
     const finalOutcome = (((salary * 0.6) + 12570) / 12).toFixed(0);
-    resultsPara.innerHTML = `£${finalOutcome}`;
+    resultsPara.innerHTML = finalOutcome;
   } else {
     // 45% off salary
     const finalOutcome = (((salary * 0.55) + 12570) / 12).toFixed(0);
-    resultsPara.innerHTML = `£${finalOutcome}`;
+    resultsPara.innerHTML = finalOutcome;
   }
+
+  const pieResult = (parseFloat(resultsPara.innerHTML) - 700)
+
+  const chartContainer = document.getElementById('my-chart');
+  const newChart = new Chart(chartContainer, {
+    type: 'doughnut',
+    data: {
+      labels: ['Travel', 'Food Shop', 'Savings', 'Gifts For Yourself', 'Going Out', 'Rent'],
+      datasets: [{
+        label: '%',
+        backgroundColor: ['#FF577B', '#36A2EB', '#73B38C', '#CC99CD', '#EE8D49', '#FFCD56'],
+        data: [(pieResult * 0.2), (pieResult * 0.2), (pieResult * 0.3), (pieResult * 0.05), (pieResult * 0.25), 700]
+      }]
+    }
+  });
   e.preventDefault();
 }
-// Create an element to prepend to "Montly take home: h6"
-// Update the salary after tax in this new element
-// Add a pie chart that gets automated with %s of what you want to spend your money on
