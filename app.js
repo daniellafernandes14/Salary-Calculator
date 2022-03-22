d8bec8f6cd7fd57b70d8ab24fa7141986e28bdbc
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-03-22 11:04:11 +0000

    updated results output to only have one outcome showing after submitting

--- app.js
+++ app.js
@@ -5,19 +5,45 @@ console.log(form);
 form.addEventListener('submit', showIncome);
 
 function showIncome(e) {
+  // Clear previous results
+  const div = document.querySelector('.results');
+  const oldPara = document.querySelector('.monthly-salary');
+
   // Select the salary input
   const salary = parseFloat(document.getElementById('salary').value);
   const allowanceRemoved = (salary - 12570);
+
+  // Create element to prepend to Monthly take home
+  const resultsTitle = document.querySelector('h6');
+  const resultsPara = document.createElement('p');
+  resultsPara.className = 'monthly-salary';
+  resultsPara.style.color = 'white';
+  resultsPara.style.fontSize = '65px';
+  resultsTitle.after(resultsPara);
+  if(oldPara) {
+    div.replaceChild(resultsPara, oldPara);
+  }
+
   if(salary <= 12570) {
     // return salary
+    const finalOutcome = (salary / 12).toFixed(0)
+    resultsPara.innerHTML = `£${finalOutcome}`;
   } else if(salary <= 50270) {
     // 20% off allowanceRemoved
+    const finalOutcome = (((allowanceRemoved * 0.8) + 12570) / 12).toFixed(0);
+    resultsPara.innerHTML = `£${finalOutcome}`;
   } else if(salary <= 125140) {
     // 40% off allowanceRemoved
+    const finalOutcome = (((allowanceRemoved * 0.6) + 12570) / 12).toFixed(0);
+    resultsPara.innerHTML = `£${finalOutcome}`;
   } else if(salary <= 150000) {
     // 40% off salary
+    const finalOutcome = (((salary * 0.6) + 12570) / 12).toFixed(0);
+    resultsPara.innerHTML = `£${finalOutcome}`;
   } else {
     // 45% off salary
+    const finalOutcome = (((salary * 0.55) + 12570) / 12).toFixed(0);
+    resultsPara.innerHTML = `£${finalOutcome}`;
   }
   e.preventDefault();
 }
